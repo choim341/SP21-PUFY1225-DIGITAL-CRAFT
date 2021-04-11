e3a97388374766ac88dd6eccc7f82e263f2b64a4
Cleo Arco <choim341@newschool.edu>
2021-04-11 20:55:37 +0900

    sketch1

---
+++ js/sketch1.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+	background(230);
+	stroke(0);
+    fill("white")
+	rect(275, 80, 15, 20);
+	rect(125, 150, 150, 100);
+	rect(275, 120, 50, 100);
+	rect(325, 160, 10, 20);
+	rect(135, 140, 20, 10);
+	rect(245, 140, 20, 10);
+	rect(185, 130, 30, 20);
+    rect(192.5, 120, 15, 10);
+	ellipse(300, 100, 90, 90);
+	ellipse(300, 100, 70, 70);
+	ellipse(200, 200, 70, 70);
+	ellipse(200, 200, 60, 60);
+    ellipse(200, 200, 30, 30);
+    ellipse(200, 200, 20, 20);
+    triangle(192, 120, 185, 130, 192, 130);
+    triangle(208, 120, 208, 130, 215, 130);
+}
+
